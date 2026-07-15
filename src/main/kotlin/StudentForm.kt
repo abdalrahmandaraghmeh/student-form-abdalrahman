a8a49af70065f981dd70a86b0b982265b92f271f
Abdalrahman <abdalrahmandaraghmeh05@gmail.com>
2026-07-15 15:39:54 +0300

    Edited student form 02

--- src/main/kotlin/StudentForm.kt
+++ src/main/kotlin/StudentForm.kt
@@ -1,6 +1,6 @@
 data class StudentForm(
     var name: String = "Abdalrahman",
-    var lastName: String = "Ina`am",
+    var lastName: String = "Ahmad",
     var age: String = "21",
     var university: String = "Philadelphia uni",
 )
